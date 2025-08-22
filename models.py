c53c48e23dea136220238649a9d27b2340ad6248
Anirudh7090 <anirudha.patil8660@gmail.com>
2025-08-22 15:32:48 +0530

    Update models.py

--- models.py
+++ models.py
@@ -4,11 +4,11 @@ from db import Base
 class Book(Base):
     __tablename__ = "books"  
 
-    id = Column(Integer, primary_key=True, index=True)  # Primary key, auto-incrementing
+    id = Column(Integer, primary_key=True, index=True)  
     title = Column(String, index=True, nullable=False)
     author = Column(String, index=True, nullable=False)
     genre = Column(String, index=True, nullable=False)
     page_count = Column(Integer, nullable=False)
     publication_year = Column(Integer, nullable=False)
     description = Column(String, nullable=False)
-    cover_image = Column(String, nullable=True)  # Optional: file path for cover image
+    cover_image = Column(String, nullable=True)  
